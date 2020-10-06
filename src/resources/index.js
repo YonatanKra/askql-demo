22558e67e41ed45436a0013442d11f19023dc1cb
yonatankra <yonatan.kra@vonage.com>
2020-10-06 18:09:02 +0300

    Setup the server and client

---
+++ src/resources/index.js
@@ -0,0 +1,18 @@
+export const values = {
+    names: [
+        'William Butcher',
+        'Hughe',
+        'M.M.',
+        'Frenchie',
+        'Starlight',
+        'Kimiko'
+    ],
+    powers: [
+        'Remorsless',
+        'Innocent',
+        'Family guy',
+        'MacGyver',
+        'Electric shocks',
+        'Strong',
+    ]
+}
