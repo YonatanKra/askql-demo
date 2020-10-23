553276e120eaf54b3d2dd049f1c6cadf9f83a325
yonatankra <yonatan.kra@vonage.com>
2020-10-23 17:48:23 +0300

    Improve the UI

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import {askMiddleware} from "./askql/askql.js";
 const port = 8080;
 const app = express();
 
+app.use(express.static('public'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
 app.post('/ask', [logRequestMiddleware, askMiddleware, logResultsMiddleware, errorHandler]);
+
 app.listen(port, () => {
     console.log(`AskQL listening at http://localhost:${port}`);
 });
